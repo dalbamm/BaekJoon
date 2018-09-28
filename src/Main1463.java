e171b9c3e09caf2e3bf28f79a042b42208fd259f
dalbamm <oscar.yoon13@gmail.com>
2018-09-28 18:55:28 +0900

    Revise 1463

--- src/Main1463.java
+++ src/Main1463.java
@@ -1,26 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main1463 {
     public static Integer [] minArr;
-    public static ArrayList<Integer>  maxArr = new ArrayList<Integer>();
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         int N = s.nextInt(),rst = 0;
          minArr= new Integer [N+1];
         minArr[1]=0;
         //need min array, min function
-        //for(int i = 0 ; i < 10 ; ++i)
-          //  System.out.println(minArr[i]);
         rst=minfun(N);
         System.out.println(rst);
         return;
     }
     static int minfun(int n){
-
         //return min(f(n-1),f(n/3),f(n/2))
         return minArr[n]==null? wk(n): minArr[n];
     }
     static int wk(int n){
-        //if(minArr[0])
         if(minArr[n]!=null) return minArr[n];
         int rst;
         if(n%3!=0 && n%2!=0)
@@ -38,14 +33,9 @@ public class Main1463 {
         return a>=b?b:a;
     }
     static int min(int a, int b, int c){
-        if(a>=b && b>=c)   return c;
-        if(a<=b && b<=c)   return a;
-        if(b>=a && a>=c)   return c;
-        if(c>=a && a>=b)   return b;
-        if(a>=c && c>=b)   return b;
-        if(b>=c && c>=a)   return a;
+        if(a>=b && b>=c)   return c;        if(a<=b && b<=c)   return a;
+        if(b>=a && a>=c)   return c;        if(c>=a && a>=b)   return b;
+        if(a>=c && c>=b)   return b;        if(b>=c && c>=a)   return a;
         return a;
     }
 }
-
-
